7750f271b19c0ee4afed3364b7cadc4f379ed5a1
zenknife <meier.scherrer@gmail.com>
2026-03-29 17:49:18 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-
-function App() {
-  return (
-    <div style={{ padding: "40px", color: "white" }}>
-      Hello. App V2 is working.
-    </div>
-  );
-}
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(<App />);
